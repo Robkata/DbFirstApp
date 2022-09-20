3fbfcfad79843b406622e9c6253dd47d025859ee
Robkata <r.georgiev2017@pmg-pernik.com>
2022-09-20 14:36:46 +0300

    A finished demo of part of a small database using the Database First method

---
+++ DbFirstDemo/DbFirstDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
